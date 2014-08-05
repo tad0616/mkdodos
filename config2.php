18915146450bfb822301e49e06a4401c3a7ede53
Tad <tad0616@gmail.com>
2014-08-05 16:32:56 +0800

    新增 mkdodos 佈景
    此佈景為馬克開發

---
+++ config2.php
@@ -0,0 +1,51 @@
+<?php
+
+
+
+// 頁首背景圖
+$theme_config[]=array(
+  "name"=>"header_img",
+  "text"=>TF_HEADER_IMG,
+  "desc"=>TF_HEADER_IMG_DESC,
+  "type"=>"file",
+  "default"=>XOOPS_URL."/themes/mkdodos/images/header.jpg",
+);
+
+// 標題文字顏色
+$theme_config[]=array(
+  "name"=>"title_color",
+  "text"=>TF_TITLE_COLOR,
+  "desc"=>TF_TITLE_COLOR_DESC,
+  "type"=>"color",
+  "default"=>"green",
+);
+
+// 副標題文字顏色
+$theme_config[]=array(
+  "name"=>"subtitle_color",
+  "text"=>TF_SUBTITLE_COLOR,
+  "desc"=>TF_SUBTITLE_COLOR_DESC,
+  "type"=>"color",
+  "default"=>"#FFFFFF",
+);
+
+
+// 頁尾文字顏色
+$theme_config[]=array(
+  "name"=>"footer_color",
+  "text"=>TF_FOOTER_COLOR,
+  "desc"=>TF_FOOTER_COLOR_DESC,
+  "type"=>"color",
+  "default"=>"#FFF",
+);
+
+// 頁尾背景色
+$theme_config[]=array(
+  "name"=>"footer_bgcolor",
+  "text"=>TF_FOOTER_BGCOLOR,
+  "desc"=>TF_FOOTER_BGCOLOR_DESC,
+  "type"=>"color",
+  "default"=>"#34A343",
+);
+
+?>
---
+++ language/english/main.php
@@ -0,0 +1,49 @@
+<?php
+define("_T2014_HELLO", "Welcom~");
+define("_T2014_HAVE_MAIL", "You have %s massage.");
+
+//選單
+define("THEME_MODULE0",	"Main menu");
+define("THEME_MODULE1",	"Home");
+
+define("TF_USER_ID",	"Username");
+define("TF_USER_PASS",	"Password");
+define("TF_USER_S_ID",	"Username:");
+define("TF_USER_S_PASS",	"Password:");
+define("TF_USER_ENTER","Login");
+define("TF_USER_EXIT","Logout");
+define("TF_USER_ADMIN","Administration");
+define("TF_USER_BLOCK","Blocks setup");
+define("TF_THEME_ADMIN","Theme setup");
+define("TF_THEME_DEBUG",	"Enable debug");
+define("TF_THEME_UNDEBUG",	"Unable debug");
+define("TF_USER_PROFILE","Profil");
+define("TF_USER_NOTICE","Notifications");
+define("TF_USER_NEWMSG","New message: ");
+define("TF_USER_MSG","Messages");
+define("TF_USER_WELCOME",	"Hello ");
+
+define("TF_SEARCH",	"Search");
+define("TF_OK",	"Submit");
+define("TF_USER_REGIST",	"Create account ?");
+define("TF_USER_FORGET_PASS",	"Password lost ?");
+
+define("TF_BLOCK_CONFIG",	"Setup this block");
+define("TF_SYSTEM_CONFIG",	"Preferences");
+define("TF_SYSTEM_MODADM",	"Online install");
+define("TF_MODULE_CONFIG",	"Administration");
+define("TF_DL_TAD_THEMES",	"Please install \"Tad Themes\" module.");
+
+/**自訂設定之語系**/
+define("TF_MY_MENU", "My Menu");
+define("TF_HEADER_IMG", "Header background image");
+define("TF_HEADER_IMG_DESC", "Header background image");
+define("TF_TITLE_COLOR", "Title color");
+define("TF_TITLE_COLOR_DESC", "Title color");
+define("TF_SUBTITLE_COLOR", "Slogqn color");
+define("TF_SUBTITLE_COLOR_DESC", "Slogqn color");
+define("TF_FOOTER_COLOR", "Footer text color");
+define("TF_FOOTER_COLOR_DESC", "Footer text color");
+define("TF_FOOTER_BGCOLOR", "Footer background color");
+define("TF_FOOTER_BGCOLOR_DESC", "Footer background color");
+?>
